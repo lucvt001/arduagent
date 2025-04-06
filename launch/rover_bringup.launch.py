1e18780093a3310eb23835c2aa0de72aa77a2740
Luc <luc001@e.ntu.edu.sg>
2025-04-06 22:19:17 +0000

    First launch commit, not tested

---
+++ launch/rover_bringup.launch.py
@@ -0,0 +1,24 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
+from launch import LaunchDescription
+
+def generate_launch_description():
+
+    config = os.path.join(get_package_share_directory('arduagent'), 'config', 'rover.yaml')
+
+    rover_node = Node(
+        name='rover',
+        executable='rover',
+        package='arduagent',
+        output='screen',
+        parameters=[config],
+        remappings=[
+            ('core/thruster_cmd_float', 'control/throttle'),
+            ('core/thrust_vector_cmd_horizontal', 'control/steering')
+        ]
+    )
+
+    return LaunchDescription([
+        rover_node,
+    ])
