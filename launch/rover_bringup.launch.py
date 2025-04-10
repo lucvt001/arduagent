d582dbd6cbbb1041f05b634fdd79ed078f7d88bb
Luc <luc001@e.ntu.edu.sg>
2025-04-10 12:53:24 +0000

    Move mqtt_client to arduagent

--- launch/rover_bringup.launch.py
+++ launch/rover_bringup.launch.py
@@ -2,9 +2,12 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import FrontendLaunchDescriptionSource
 
 def generate_launch_description():
 
+    param_file_name = 'mqtt_params_leader.yaml'
     config = os.path.join(get_package_share_directory('arduagent'), 'config', 'rover.yaml')
 
     rover_node = Node(
@@ -15,6 +18,13 @@ def generate_launch_description():
         parameters=[config]
     )
 
+    mqtt_client = IncludeLaunchDescription(
+        FrontendLaunchDescriptionSource(
+            os.path.join(get_package_share_directory('mqtt_client'), 'launch', 'standalone.launch.ros2.xml')
+        ), launch_arguments={'params_file': os.path.join(get_package_share_directory('arduagent'), 'config', param_file_name)}.items()
+    )
+
     return LaunchDescription([
         rover_node,
+        TimerAction(period=2.0, actions=[mqtt_client]),
     ])
